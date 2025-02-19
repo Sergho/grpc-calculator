1498f21aabbc752982987ca0b66efaecb91e5480
Sergho <sergche04@gmail.com>
2025-02-19 20:12:36 +0000

    feat: modify server to be as calculator

--- server.cpp
+++ server.cpp
@@ -1,33 +1,50 @@
 #include <iostream>
 #include <grpc++/grpc++.h>
-#include "ping.grpc.pb.h"
+#include "calculator.grpc.pb.h"
 
 using grpc::Server;
 using grpc::Status;
+using grpc::StatusCode;
 using grpc::ServerContext;
 using grpc::ServerBuilder;
-using grpc::InsecureServerCredentials;
 
 using std::cout;
 using std::endl;
 using std::unique_ptr;
 
-class PingImpl : public Ping::Service {
-	Status ping(ServerContext* context, const Msg* request, Msg* response) {
-		cout << request->payload() << endl;
-		response->set_payload(request->payload());
+class CalculatorImpl : public Calculator::Service {
+	Status Addition(ServerContext* context, const OperandsRequest* request, Response* response) {
+		response->set_result(request->first() + request->second());
+		return Status::OK;
+	}
+    Status Subtraction(ServerContext* context, const OperandsRequest* request, Response* response) {
+		response->set_result(request->first() - request->second());
+		return Status::OK;
+	}
+	Status Multiplication(ServerContext* context, const OperandsRequest* request, Response* response) {
+		response->set_result(request->first() * request->second());
+		return Status::OK;
+	}
+	Status Division(ServerContext* context, const OperandsRequest* request, Response* response) {
+		if(request->second() == 0) return Status(StatusCode::INVALID_ARGUMENT, "Division by zero");
+		response->set_result(request->first() / request->second());
 		return Status::OK;
 	}
 };
 
-int main() {
-	PingImpl service;
+unique_ptr<Server> SetupServer() {
+	CalculatorImpl service;
 	ServerBuilder builder;
-	builder.AddListeningPort("0.0.0.0:4000", InsecureServerCredentials());
+	builder.AddListeningPort("0.0.0.0:4000", grpc::InsecureServerCredentials());
 	builder.RegisterService(&service);
 
 	unique_ptr<Server> server(builder.BuildAndStart());
-	server->Wait();
+	return server;
+}
 
+int main() {
+	unique_ptr<Server> server = SetupServer();
+	server->Wait();
+	
 	return 0;
 }
