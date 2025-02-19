33352bcc6d8386943982003c17ce4536a4277cdc
Sergho <sergche04@gmail.com>
2025-02-19 14:47:43 +0000

    feat: create gRPC simple echo client

--- client.cpp
+++ client.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
-#include <grpc/grpc.h>
+#include <grpc++/grpc++.h>
+#include "ping.grpc.pb.h"
+
+using grpc::ClientContext;
+using grpc::Channel;
+using grpc::CreateChannel;
+using grpc::InsecureChannelCredentials;
+using grpc::Status;
+
+using std::cout;
+using std::endl;
+using std::shared_ptr;
+using std::unique_ptr;
 
 int main() {
-	std::cout << "Client" << std::endl;
+	Msg request, response;
+	request.set_payload("Hello, server!");
+
+	ClientContext context;
+	std::shared_ptr<Channel> channel = grpc::CreateChannel("localhost:4000", InsecureChannelCredentials());
+	unique_ptr<Ping::Stub> stub = Ping::NewStub(channel);
+
+	Status status = stub->ping(&context, request, &response);
+
+	if (status.ok()) {
+		cout << response.payload() << endl;
+	} 
+	else {
+		cout << status.error_code() << ": " << status.error_message() << endl;
+	}
+
 	return 0;
 }
